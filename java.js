39efe487ef53753f22979fb7274ee44e907ed0c6
AmrFiqi <amrfiqi55@gmail.com>
2022-04-15 15:29:25 +0200

    Testing on linux

--- java.js
+++ java.js
@@ -5,10 +5,7 @@ function tabs(){
            const tabData = sideTabs.parentElement;
            const tabNum= button.dataset.forTab;
            const active= tabData.querySelector(`.data[data-tab='${tabNum}']`);
-           console.log(sideTabs);
-           console.log(tabData);
-           console.log(tabNum);    
-           console.log(active);
+           
 
            sideTabs.querySelectorAll(".tabs_button").forEach(button =>{
                button.classList.remove("tabs_button-active");
